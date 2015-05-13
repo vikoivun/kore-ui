444c625fcafdac65ecb87e3aa9d4f982f669e622
Lauri Raivio <lauri.raivio@gmail.com>
2015-05-13 08:10:30 +0300

    Improve webpack configs
    Refs #15.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+var config = require('./conf/webpack.config');
 
 var server = new WebpackDevServer(webpack(config), {
   historyApiFallback: true,
