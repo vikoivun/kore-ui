41d6bab0e5dba50ff2fad4723941333f876bc8d2
Lauri Raivio <lauri.raivio@gmail.com>
2015-05-13 10:49:15 +0300

    Add configurations for karma and tests
    Refs #2.

---
+++ conf/karma.coverage.conf.js
@@ -0,0 +1,10 @@
+/*eslint-disable no-var*/
+'use strict';
+
+var karmaFactory = require('./make-karma-config.js');
+
+module.exports = function(config) {
+  config.set(karmaFactory({
+    coverage: true
+  }));
+};
