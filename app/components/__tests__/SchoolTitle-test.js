b37213d9c7ea50aca4070c2c8c3d9ae4e7284f29
Lauri Raivio <lauri.raivio@gmail.com>
2015-05-19 11:36:28 +0300

    Add tests for SchoolTitle component

--- app/components/__tests__/SchoolTitle-test.js
+++ app/components/__tests__/SchoolTitle-test.js
@@ -6,23 +6,51 @@ import { expect } from 'chai';
 import SchoolTitle from '../SchoolTitle';
 
 describe('SchoolTitle', function() {
-  it('renders', () => {
-    const name = {'official_name': 'River Valley High'};
-    const yearsActive = {
-      'beginYear': 1995,
-      'endYear': 2010
-    };
+  const name = {'official_name': 'River Valley High'};
+  const yearsActive = {
+    'beginYear': 1995,
+    'endYear': 2010
+  };
+  let element;
 
-    const element = TestUtils.renderIntoDocument(
+  before(function() {
+    element = TestUtils.renderIntoDocument(
       <SchoolTitle
         name={name}
         yearsActive={yearsActive}
-      />);
+      />
+    );
+  });
+
+  it('should render without problems', function() {
     expect(element).to.be.ok;
   });
 
-  it('should always pass this test', function() {
-    const a = 2;
-    expect(a).to.equal(2);
+  it('should display the component', function() {
+    const component = TestUtils.scryRenderedDOMComponentsWithClass(element, 'school-title');
+    expect(component.length).to.equal(1);
+  });
+
+  it('should display school name in h1 tag', function() {
+    const header = TestUtils.findRenderedDOMComponentWithTag(element, 'h1');
+    const dom = React.findDOMNode(header);
+    expect(dom.textContent).to.equal(name.official_name);
+  });
+
+  it('should display the year school began operation', function() {
+    const yearSpan = TestUtils.findRenderedDOMComponentWithTag(element, 'span');
+    const dom = React.findDOMNode(yearSpan);
+    expect(dom.textContent).to.contain(yearsActive.beginYear);
+  });
+
+  it('should display the year school ended operation', function() {
+    const yearSpan = TestUtils.findRenderedDOMComponentWithTag(element, 'span');
+    const dom = React.findDOMNode(yearSpan);
+    expect(dom.textContent).to.contain(yearsActive.endYear);
+  });
+
+  it('should display an arrow between the school years', function() {
+    const arrow = TestUtils.scryRenderedDOMComponentsWithClass(element, 'fa-long-arrow-right');
+    expect(arrow.length).to.equal(1);
   });
 });
