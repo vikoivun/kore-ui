04f01457a5e2feefb42b6e0a05bd60c152e7b2c8
Lauri Raivio <lauri.raivio@gmail.com>
2015-05-22 16:34:07 +0300

    Refactor SchoolStore

--- app/stores/SchoolStore.js
+++ app/stores/SchoolStore.js
@@ -8,132 +8,19 @@ import BaseStore from './BaseStore';
 let _schools = {};
 let _fetchingData = false;
 
-const getSchoolByIdWrapper = function(func, defaultValue) {
-  return function(schoolId) {
-    if (!defaultValue) {
-      defaultValue = [];
-    }
-    const school = this.getSchool(schoolId);
-    if (_.isEmpty(school)) {
-      return defaultValue;
-    }
-    return func(school);
-  };
-};
-
 const SchoolStore = Object.assign({}, BaseStore, {
-  getFetchingData: function() {
-    return _fetchingData;
-  },
-
-  getSchool: function(schoolId) {
-    return _schools[schoolId] || {};
-  },
-
-  getSchools: function(schoolIds) {
-    return _.map(schoolIds, function(id) {
-      if (!_schools[id].buildings.length) {
-        return {
-          id: id,
-          name: this.getMainName(id).official_name,
-          address: ''
-        };
-      }
-      const addresses = this.getMainBuilding(id).building.addresses;
-      const address = (
-        addresses.length ?
-        `${addresses[0].street_name_fi}\, ${addresses[0].municipality_fi}` :
-        ''
-      );
-      return {
-        id: id,
-        name: this.getMainName(id).official_name,
-        address: address
-      };
-    }, this);
-  },
-
-  hasSchool: function(schoolId) {
-    return typeof _schools[schoolId] !== 'undefined';
-  },
-
-  getMainName: function(schoolId) {
-    let sortedSchoolNames = this.getSchoolNames(schoolId);
-    if (sortedSchoolNames.length) {
-      return sortedSchoolNames[0];
-    }
-    return {};
-  },
-
-  getMainBuilding: function(schoolId) {
-    let sortedBuildings = this.getBuildings(schoolId);
-    if (sortedBuildings.length) {
-      return sortedBuildings[0];
-    }
-    return {};
-  },
-
-  getSchoolNames: getSchoolByIdWrapper(function(school) {
-    return _.sortBy(school.names, function(name) {
-      return -name.begin_year;
-    });
-  }),
-
-  getBuildings: getSchoolByIdWrapper(function(school) {
-    return _.sortBy(school.buildings, function(building) {
-      return -building.begin_year;
-    });
-  }),
-
-  getMainBuildingInYear: function(schoolId, year) {
-    if (!year) {
-      return this.getMainBuilding(schoolId);
-    }
-    const buildings = this.getBuildings(schoolId);
-    const building = _.find(buildings, function(build) {
-      return build.begin_year <= year;
-    });
-    return building || {};
-  },
-
-  getSchoolDetails: function(schoolId) {
-    return {
-      schoolNames: this.getSchoolNames(schoolId),
-      buildings: this.getBuildings(schoolId)
-    };
-  },
-
-  getSchoolYearDetails: function(schoolId, year) {
-    const schoolNames = this.getSchoolNames(schoolId);
-    const buildings = this.getBuildings(schoolId);
-    if (!year) {
-      year = new Date().getFullYear();
-    }
-    const schoolName = _.find(schoolNames, function(school) {
-      return school.begin_year <= year;
-    });
-    const building = _.find(buildings, function(build) {
-      return build.begin_year <= year;
-    });
-    return {
-      schoolName: schoolName || {},
-      building: building || {}
-    };
-  },
-
-  calculateBeginAndEndYear: function(schoolId) {
-    let sortedNames = this.getSchoolNames(schoolId);
-    let beginYear;
-    let endYear;
-    if (sortedNames.length) {
-      beginYear = sortedNames[sortedNames.length - 1].begin_year;
-      endYear = sortedNames[0].end_year;
-    }
-    return {
-      beginYear: beginYear,
-      endYear: endYear
-    };
-  }
+  calculateBeginAndEndYear,
+  getBuildings,
+  getFetchingData,
+  getMainBuilding,
+  getMainBuildingInYear,
+  getMainName,
+  getSchool,
+  getSchoolDetails,
+  getSchoolNames,
+  getSchoolYearDetails,
+  getSchools,
+  hasSchool
 });
 
 SchoolStore.dispatchToken = AppDispatcher.register(function(payload) {
@@ -165,6 +52,133 @@ SchoolStore.dispatchToken = AppDispatcher.register(function(payload) {
       // noop
   }
 });
+
+const getBuildings = _getSchoolByIdWrapper(function(school) {
+  return _.sortBy(school.buildings, function(building) {
+    return -building.begin_year;
+  });
+});
+
+const getSchoolNames = _getSchoolByIdWrapper(function(school) {
+  return _.sortBy(school.names, function(name) {
+    return -name.begin_year;
+  });
+});
+
+function calculateBeginAndEndYear(schoolId) {
+  let sortedNames = getSchoolNames(schoolId);
+  let beginYear;
+  let endYear;
+  if (sortedNames.length) {
+    beginYear = sortedNames[sortedNames.length - 1].begin_year;
+    endYear = sortedNames[0].end_year;
+  }
+  return {
+    beginYear: beginYear,
+    endYear: endYear
+  };
+}
+
+function getFetchingData() {
+  return _fetchingData;
+}
+
+function getMainBuilding(schoolId) {
+  let sortedBuildings = getBuildings(schoolId);
+  if (sortedBuildings.length) {
+    return sortedBuildings[0];
+  }
+  return {};
+}
+
+function getMainBuildingInYear(schoolId, year) {
+  if (!year) {
+    return getMainBuilding(schoolId);
+  }
+  const buildings = getBuildings(schoolId);
+  const building = _.find(buildings, function(build) {
+    return build.begin_year <= year;
+  });
+  return building || {};
+}
+
+function getMainName(schoolId) {
+  let sortedSchoolNames = getSchoolNames(schoolId);
+  if (sortedSchoolNames.length) {
+    return sortedSchoolNames[0];
+  }
+  return {};
+}
+
+function getSchool(schoolId) {
+  return _schools[schoolId] || {};
+}
+
+function getSchoolDetails(schoolId) {
+  return {
+    schoolNames: getSchoolNames(schoolId),
+    buildings: getBuildings(schoolId)
+  };
+}
+
+function getSchools(schoolIds) {
+  return _.map(schoolIds, function(id) {
+    if (!_schools[id].buildings.length) {
+      return {
+        id: id,
+        name: this.getMainName(id).official_name,
+        address: ''
+      };
+    }
+
+    const addresses = getMainBuilding(id).building.addresses;
+    const address = (
+      addresses.length ?
+      `${addresses[0].street_name_fi}\, ${addresses[0].municipality_fi}` :
+      ''
+    );
+    return {
+      id: id,
+      name: getMainName(id).official_name,
+      address: address
+    };
+  }, this);
+}
+
+function getSchoolYearDetails(schoolId, year) {
+  const schoolNames = getSchoolNames(schoolId);
+  const buildings = getBuildings(schoolId);
+  if (!year) {
+    year = new Date().getFullYear();
+  }
+  const schoolName = _.find(schoolNames, function(school) {
+    return school.begin_year <= year;
+  });
+  const building = _.find(buildings, function(build) {
+    return build.begin_year <= year;
+  });
+  return {
+    schoolName: schoolName || {},
+    building: building || {}
+  };
+}
+
+function hasSchool(schoolId) {
+  return typeof _schools[schoolId] !== 'undefined';
+}
+
+function _getSchoolByIdWrapper(func, defaultValue) {
+  return function(schoolId) {
+    if (!defaultValue) {
+      defaultValue = [];
+    }
+    const school = getSchool(schoolId);
+    if (_.isEmpty(school)) {
+      return defaultValue;
+    }
+    return func(school);
+  };
+}
 
 function _receiveSchool(school) {
   _schools[school.id] = school;
