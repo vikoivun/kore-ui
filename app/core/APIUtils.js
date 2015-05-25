8827cb5fa4f1c5b3caae5d6994fe52811f2dacf0
Lauri Raivio <lauri.raivio@gmail.com>
2015-05-25 18:40:11 +0300

    Add building store

--- app/core/APIUtils.js
+++ app/core/APIUtils.js
@@ -2,18 +2,25 @@
 
 import {Schema, arrayOf, normalize} from 'normalizr';
 
+const building = new Schema('buildings');
 const principal = new Schema('principals');
+const schoolBuilding = new Schema('schoolBuildings');
 const schoolPrincipal = new Schema('schoolPrincipals');
 const school = new Schema('schools');
 // We need to define a searchRespone shema so normalizr finds all entities from
 // the search results.
 const searchResponse = new Schema('searchResponse');
 
+schoolBuilding.define({
+  building: building
+});
+
 schoolPrincipal.define({
   principal: principal
 });
 
 school.define({
+  buildings: arrayOf(schoolBuilding),
   principals: arrayOf(schoolPrincipal)
 });
 
---
+++ app/stores/BuildingStore.js
@@ -0,0 +1,62 @@
+'use strict';
+
+import _ from 'lodash';
+import AppDispatcher from '../core/AppDispatcher';
+import ActionTypes from '../constants/ActionTypes';
+import BaseStore from './BaseStore';
+
+let _fetchingData = false;
+let _buildings = {};
+
+const BuildingStore = Object.assign({}, BaseStore, {
+  getFetchingData,
+  getBuilding: _getBuildingByIdWrapper(getBuilding, {})
+});
+
+BuildingStore.dispatchToken = AppDispatcher.register(function(payload) {
+  const action = payload.action;
+
+  switch (action.type) {
+
+    case ActionTypes.REQUEST_SCHOOL:
+      _fetchingData = true;
+      BuildingStore.emitChange();
+      break;
+
+    case ActionTypes.REQUEST_SCHOOL_SUCCESS:
+      _fetchingData = false;
+      _receiveBuildings(action.response.entities.buildings);
+      BuildingStore.emitChange();
+      break;
+
+    case ActionTypes.REQUEST_SEARCH_SUCCESS:
+      _receiveBuildings(action.response.entities.buildings);
+      BuildingStore.emitChange();
+      break;
+
+    default:
+      // noop
+  }
+});
+
+function getFetchingData() {
+  return _fetchingData;
+}
+
+function getBuilding(building) {
+  return building;
+}
+
+function _getBuildingByIdWrapper(func, defaultValue) {
+  return function(buildingId) {
+    defaultValue = defaultValue ? defaultValue : [];
+    const building = _buildings[buildingId];
+    return _.isEmpty(building) ? defaultValue : func(building);
+  };
+}
+
+function _receiveBuildings(buildings) {
+  _.assign(_buildings, buildings);
+}
+
+export default BuildingStore;
